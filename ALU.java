beff13b0cc54d9cac2612edbda1935a06d4b5a56
CreatorOfCryptids <dannypeck5000@gmail.com>
2024-02-05 00:58:28 -0500

    Commented out bad code. Still need to add good code.

--- ALU.java
+++ ALU.java
@@ -8,14 +8,33 @@ public class ALU {
         result = new Word();
     }
     
+    private enum op{
+        AND,        // 1000
+        OR,         // 1001
+        XOR,        // 1010
+        NOT,        // 1011
+        LEFT,       // 1100
+        RIGHT,      // 1101
+        ADD,        // 1110
+        SUBTRACT,   // 1111
+        MULTIPLY    // 0111
+    }
+
+
+
     /**
      * Performs the operation encoded by the passed Code Operation.
      * @param operation An array of 4 Bits 
      * @throws Exception
      */
     public void doOperation(Bit[] operation) throws Exception{
+
+        switch(){
+            case
+        }
+        throw new Exception("Unexpected code " + operation[0].toString() + operation[1].toString() + operation[2].toString() + operation[3].toString());
         
-        if(operation.length != 4)
+        /*if(operation.length != 4)
             throw new Exception("Wrong number of operation bits");
 
         byte code = opCodeToNum(operation);
@@ -30,7 +49,7 @@ public class ALU {
         else if (code == 12){   // LeftShift
             result.copy(op1);
             int shift = 1;
-            for(int i = 1; i<Word.WORD_SIZE; i++){
+            for(int i = 1; i<5; i++){
                 result.leftShift(shift);
                 shift *= 2;
             }
@@ -38,7 +57,7 @@ public class ALU {
         else if (code == 13){   // RightShift
             result.copy(op1);
             int shift = 1;
-            for(int i = 1; i<Word.WORD_SIZE; i++){
+            for(int i = 1; i<5; i++){
                 result.rightShift(shift);
                 shift *= 2;
             }
@@ -51,13 +70,14 @@ public class ALU {
             multiply();
         else
             throw new Exception("Unexpected code " + opCodeToNum(operation));
+        */
     }
 
     /**
      * Turns the CodeOperation into a number for easy switching.
      * @param operation
      * @return
-     */
+     *
     public byte opCodeToNum(Bit[] operation){
         byte retval = 0;
 
@@ -71,7 +91,7 @@ public class ALU {
             retval+=1;
 
         return retval;
-    }
+    }/**/
 
     /**
      * Calls add2 to add the two words together.
@@ -149,7 +169,64 @@ public class ALU {
      * Multiplys the two words together.
      */
     private void multiply() throws Exception{
-        result = add2(add4(add4(op2.getBit(0).getValue() ? op1.leftShift(0) : new Word(),op2.getBit(1).getValue() ? op1.leftShift(1) : new Word(),op2.getBit(2).getValue() ? op1.leftShift(2) : new Word(),op2.getBit(3).getValue() ? op1.leftShift(3) : new Word()),add4( op2.getBit(4).getValue() ? op1.leftShift(4) : new Word(),op2.getBit(5).getValue() ? op1.leftShift(5) : new Word(),op2.getBit(6).getValue() ? op1.leftShift(6) : new Word(),op2.getBit(7).getValue() ? op1.leftShift(7) : new Word()),add4( op2.getBit(8).getValue() ? op1.leftShift(8) : new Word(),op2.getBit(9).getValue() ? op1.leftShift(9) : new Word(),op2.getBit(10).getValue() ? op1.leftShift(10) : new Word(),op2.getBit(11).getValue() ? op1.leftShift(11) : new Word()),add4( op2.getBit(12).getValue() ? op1.leftShift(12) : new Word(),op2.getBit(13).getValue() ? op1.leftShift(13) : new Word(),op2.getBit(14).getValue() ? op1.leftShift(14) : new Word(),op2.getBit(15).getValue() ? op1.leftShift(15) : new Word())),add4(add4( op2.getBit(16).getValue() ? op1.leftShift(16) : new Word(),op2.getBit(17).getValue() ? op1.leftShift(17) : new Word(),op2.getBit(18).getValue() ? op1.leftShift(18) : new Word(),op2.getBit(19).getValue() ? op1.leftShift(19) : new Word()),add4(op2.getBit(20).getValue() ? op1.leftShift(20) : new Word(),op2.getBit(21).getValue() ? op1.leftShift(21) : new Word(),op2.getBit(22).getValue() ? op1.leftShift(22) : new Word(),op2.getBit(23).getValue() ? op1.leftShift(23) : new Word()),add4(op2.getBit(24).getValue() ? op1.leftShift(24) : new Word(),op2.getBit(25).getValue() ? op1.leftShift(25) : new Word(),op2.getBit(26).getValue() ? op1.leftShift(26) : new Word(),op2.getBit(27).getValue() ? op1.leftShift(27) : new Word()),add4(op2.getBit(28).getValue() ? op1.leftShift(28) : new Word(),op2.getBit(29).getValue() ? op1.leftShift(29) : new Word(),op2.getBit(30).getValue() ? op1.leftShift(30) : new Word(),op2.getBit(31).getValue() ? op1.leftShift(31) : new Word())));
+
+        
+
+        /*result = 
+        add2(
+            add4( 
+                add4( 
+                    op2.getBit(0).getValue() ? op1.leftShift(0) : new Word(),
+                    op2.getBit(1).getValue() ? op1.leftShift(1) : new Word(),
+                    op2.getBit(2).getValue() ? op1.leftShift(2) : new Word(),
+                    op2.getBit(3).getValue() ? op1.leftShift(3) : new Word()
+                ),
+                add4( 
+                    op2.getBit(4).getValue() ? op1.leftShift(4) : new Word(),
+                    op2.getBit(5).getValue() ? op1.leftShift(5) : new Word(),
+                    op2.getBit(6).getValue() ? op1.leftShift(6) : new Word(),
+                    op2.getBit(7).getValue() ? op1.leftShift(7) : new Word()
+                ),
+                add4( 
+                    op2.getBit(8).getValue() ? op1.leftShift(8) : new Word(),
+                    op2.getBit(9).getValue() ? op1.leftShift(9) : new Word(),
+                    op2.getBit(10).getValue() ? op1.leftShift(10) : new Word(),
+                    op2.getBit(11).getValue() ? op1.leftShift(11) : new Word()
+                ),
+                add4( 
+                    op2.getBit(12).getValue() ? op1.leftShift(12) : new Word(),
+                    op2.getBit(13).getValue() ? op1.leftShift(13) : new Word(),
+                    op2.getBit(14).getValue() ? op1.leftShift(14) : new Word(),
+                    op2.getBit(15).getValue() ? op1.leftShift(15) : new Word()
+                )
+            ),
+            add4( 
+                add4( 
+                    op2.getBit(16).getValue() ? op1.leftShift(16) : new Word(),
+                    op2.getBit(17).getValue() ? op1.leftShift(17) : new Word(),
+                    op2.getBit(18).getValue() ? op1.leftShift(18) : new Word(),
+                    op2.getBit(19).getValue() ? op1.leftShift(19) : new Word()
+                ),
+                add4( 
+                    op2.getBit(20).getValue() ? op1.leftShift(20) : new Word(),
+                    op2.getBit(21).getValue() ? op1.leftShift(21) : new Word(),
+                    op2.getBit(22).getValue() ? op1.leftShift(22) : new Word(),
+                    op2.getBit(23).getValue() ? op1.leftShift(23) : new Word()
+                ),
+                add4( 
+                    op2.getBit(24).getValue() ? op1.leftShift(24) : new Word(),
+                    op2.getBit(25).getValue() ? op1.leftShift(25) : new Word(),
+                    op2.getBit(26).getValue() ? op1.leftShift(26) : new Word(),
+                    op2.getBit(27).getValue() ? op1.leftShift(27) : new Word()
+                ),
+                add4( 
+                    op2.getBit(28).getValue() ? op1.leftShift(28) : new Word(),
+                    op2.getBit(29).getValue() ? op1.leftShift(29) : new Word(),
+                    op2.getBit(30).getValue() ? op1.leftShift(30) : new Word(),
+                    op2.getBit(31).getValue() ? op1.leftShift(31) : new Word()
+                )
+            )
+        );*/
     }
 
     /**
