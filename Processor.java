423e424335a1b16a92ac26c40990ded74d359e65
CreatorOfCryptids <dannypeck5000@gmail.com>
2024-02-12 23:56:27 -0500

    Started on Processor.java.

---
+++ Processor.java
@@ -0,0 +1,28 @@
+public class Processor {
+
+    Word PC;    // Program Counter
+    Word SP;    // Stack Pointer.
+
+    Processor(){
+        PC = new Word();
+        SP = new Word();
+
+        SP.set(1024);
+    }
+
+    public void fetch(){
+
+    }
+
+    public void decode(){
+
+    }
+
+    public void execute(){
+
+    }
+
+    public void store(){
+
+    }
+}
