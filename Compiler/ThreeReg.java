bb9ddc707f1ac2c4162a9c8076659ffcd1000d6b
CreatorOfCryptids <dannypeck5000@gmail.com>
2024-04-10 16:56:33 -0400

    Made the no imm constructor use a preset string instead of the immToStirng() method.

--- Compiler/ThreeReg.java
+++ Compiler/ThreeReg.java
@@ -3,30 +3,12 @@ package Compiler;
 public class ThreeReg extends Instruction{
 
     protected static int IMM_SIZE = 8;
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 94b3465 (Fixed visibility in ThreeReg)
     private String rd;
     private String func;
     private String rs2;
     private String rs1;
-<<<<<<< HEAD
 
     public ThreeReg(Instruction.OpCode code, int rd, Instruction.Function func, int rs2, int rs1, int imm){
-=======
-    String rd;
-    String func;
-    String rs2;
-    String rs1;
-
-    ThreeReg(Instruction.OpCode code, int rd, Instruction.Function func, int rs2, int rs1, int imm){
->>>>>>> d35383a (Created the three instruction class.)
-=======
-
-    public ThreeReg(Instruction.OpCode code, int rd, Instruction.Function func, int rs2, int rs1, int imm){
->>>>>>> 94b3465 (Fixed visibility in ThreeReg)
         this.op = opToString[code.ordinal()] + "11";
         this.rd = regMap[rd];
         this.func = funcToString[func.ordinal()];
@@ -35,25 +17,13 @@ public class ThreeReg extends Instruction{
         this.imm = immToString(imm, IMM_SIZE);
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     public ThreeReg(Instruction.OpCode code, int rd, Instruction.Function func, int rs2, int rs1){
-=======
-    ThreeReg(Instruction.OpCode code, int rd, Instruction.Function func, int rs2, int rs1){
->>>>>>> d35383a (Created the three instruction class.)
-=======
-    public ThreeReg(Instruction.OpCode code, int rd, Instruction.Function func, int rs2, int rs1){
->>>>>>> 94b3465 (Fixed visibility in ThreeReg)
         this.op = opToString[code.ordinal()] + "11";
         this.rd = regMap[rd];
         this.func = funcToString[func.ordinal()];
         this.rs2 = regMap[rs2];
         this.rs1 = regMap[rs1];
-<<<<<<< HEAD
         this.imm = "00000000";
-=======
-        this.imm = immToString(0, IMM_SIZE);
->>>>>>> d35383a (Created the three instruction class.)
     }
 
     public String toInstruction() {
