d0b6acea8b4f5eb8e603e2c54b19b18df47c0105
CreatorOfCryptids <dannypeck5000@gmail.com>
2024-04-15 19:24:41 -0400

    Added Javadocs

--- Compiler/Token.java
+++ Compiler/Token.java
@@ -14,24 +14,50 @@ public class Token {
     private Type type;
     private Optional<Integer> value;
 
+    /**
+     * Constructor
+     * 
+     * @param type The Token.Type of the new Token.
+     * @param value The integer value of the new Token (Only used for registers and immediate)
+     */
     public Token(Type type, int value){
         this.type = type;
         this.value = Optional.of(value);
     }
 
+    /**
+     * Value free Constructor.
+     * 
+     * @param type The Token.Type of the new Token.
+     */
     public Token(Type type){
         this.type = type;
         this.value = Optional.empty();
     }
 
+    /**
+     * Gets the Token's Type
+     * 
+     * @return The type of the Token.
+     */
     public Type getType(){
         return type;
     }
 
+    /**
+     * Gets the value of the Token
+     * 
+     * @return An Optional<Integer> containing the value of the Token.
+     */
     public Optional<Integer> getValue(){
         return value;
     }
 
+    /**
+     * Returns a String to view the tokens for easy debugging.
+     * 
+     * @return A String containing the important information in the Token
+     */
     public String toString(){
         return type.toString() + "" + (value.isPresent() ? "<" + value.get() + ">" : "");
     }
