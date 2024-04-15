685a3e9174edac5a8719cea30a99506e25163bb6
CreatorOfCryptids <dannypeck5000@gmail.com>
2024-04-15 18:05:13 -0400

    Fixed input and added comments.

--- Compiler/Compiler.java
+++ Compiler/Compiler.java
@@ -9,26 +9,42 @@ public class Compiler {
 
     private String[] input;
 
+    /**
+     * Compiles the assembly source code in the passed file.
+     * 
+     * @param args 
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception{
 
         Compiler comp;
 
-        if (args.length >0 )
+        if (args.length == 1)
             comp = new Compiler(args[0]);
         else
             throw new Exception("INCORRECT ARGS: Missing inputFileName.");
         
-        for(String s : comp.compile()){
+        for(String s : comp.compile())
             System.out.println(s);
-        }
     }
 
+    /**
+     * Constructor.
+     * 
+     * @param inputFileName The name of the file that contains the assembly source code.
+     * @throws Exception
+     */
     public Compiler(String inputFileName) throws Exception{
         Path myPath = Paths.get(inputFileName);
         input = ((String[])Files.readAllLines(myPath).toArray());
-
     }
 
+    /**
+     * Compiles the input data into SIA32 binary instructions.
+     * 
+     * @return An array of String containing the compiled SIA32 bits in '0's and '1's
+     * @throws Exception
+     */
     public String[] compile() throws Exception{
         Lexer lexer = new Lexer(input);
 
