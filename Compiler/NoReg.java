08dd11ab516a1f33a9781d93ab85b72980b3f39f
CreatorOfCryptids <dannypeck5000@gmail.com>
2024-04-15 19:15:50 -0400

    Added Javadocs

--- Compiler/NoReg.java
+++ Compiler/NoReg.java
@@ -4,16 +4,32 @@ public class NoReg extends Instruction{
 
     protected static final int IMM_SIZE = 27;
 
+    /**
+     * Constructor.
+     * 
+     * @param code The OpCode of the Instruction.
+     * @param imm The immediate value of the instruction.
+     */
     public NoReg(Instruction.OpCode code, int imm){
         this.op = code;
         this.imm = imm;
     }
 
+    /**
+     * Constructor.
+     * 
+     * @param code The OpCode of the Instruction.
+     */
     public NoReg(Instruction.OpCode code){
         this.op = code;
         this.imm = 0;
     }
 
+    /**
+     * Copy Constructor.
+     * 
+     * @param instruction The Old Instruction.
+     */
     public NoReg(NoReg instruction){
         this.op = instruction.op;
         this.imm = instruction.imm;
