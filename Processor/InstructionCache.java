91c65c54b45ea910577b47a56b32821e07ca29cb
CreatorOfCryptids <dannypeck5000@gmail.com>
2024-04-28 19:12:34 -0400

    Polished and compleated. NO MORE CODING FOR 404!!!

--- Processor/InstructionCache.java
+++ Processor/InstructionCache.java
@@ -2,23 +2,22 @@ package Processor;
 
 public class InstructionCache {
     
-    public static final int CACHE_SIZE = 8;
+    public static final int CACHE_SIZE = 8; // The amount of words that this cache can hold.
     private static Word[] cache = new Word[CACHE_SIZE];
-    private static int startAddress = -10;
+    private static int startAddress = -2 - CACHE_SIZE;  // The inital starting address. Set to negative so that accessing 0 doesn't return a null value.
 
     /**
      * Gets the requested instruction through a cache.
      * 
-     * Cache hit costs 10, cache miss costs 350.
+     * Cache hit costs 10, cache misses cost 350.
      * 
      * @param addressWord The address of the requested instruction.
      * @return The requested instruction.
      * @throws Exception
      */
     public static Word readInstruction(Word addressWord) throws Exception{
-        int address = addressWord.getSigned();
-
         Processor.clockCycles += 10;
+        int address = addressWord.getSigned();
 
         // If address is not in cache...
         if (address < startAddress || address >= startAddress + CACHE_SIZE){
@@ -38,6 +37,9 @@ public class InstructionCache {
         return cache[address-startAddress];
     }
 
+    /**
+     * Clears the cache for use by another process.
+     */
     public static void clear(){
         for(int i=0; i< CACHE_SIZE; i++){
             cache[i] = new Word();
--- Processor/L2Cache.java
+++ Processor/L2Cache.java
@@ -1,24 +1,32 @@
 package Processor;
 
 public class L2Cache{
-    public static int CACHE_COUNT = 4;
-    public static int CACHE_SIZE = 8;
+    public static int CACHE_COUNT = 4;  // The amount of caches.
+    public static int CACHE_SIZE = 8;   // The amount of Words in each cache.
     private static Word[][] cache = new Word[CACHE_COUNT][CACHE_SIZE];
     private static int[] addresses = new int[CACHE_COUNT];
-    private static int nextCache = 0;
+    private static int nextCache = 0;   // Stores the index of the next cache.
 
+    /**
+     * Reads The designated address in memory. 
+     * 
+     * Costs 20 cycles on a cache hit, and 350 on a miss.
+     * 
+     * @param address The address of the desired memory.
+     * @return The memory stored in the specified address.
+     * @throws Exception
+     */
     public static Word read(Word address) throws Exception{
-
-        int addr = address.getSigned();
-
         Processor.clockCycles += 20;
 
+        int addr = address.getSigned();
         int index = searchAddresses(addr);
 
         if (index == -1){
-
+            // Already added 20, so we only need to add 330 cycles.
             Processor.clockCycles += 330;
 
+            // Get the next cache group in the rotation. Makes a loop from 0-3 infinitly.
             index = nextCache++ % CACHE_COUNT;
             addresses[index] = addr;
 
@@ -27,15 +35,23 @@ public class L2Cache{
             }
         }
 
-        return cache[index][addr- addresses[index]];
+        return cache[index][addr - addresses[index]];
     }
 
+    /**
+     * Writes a value to the specified address in memory.
+     * 
+     * Costs 50 cycles.
+     * 
+     * @param address The address to be writen to.
+     * @param value The new value for the speficied address.
+     * @throws Exception
+     */
     public static void write(Word address, Word value) throws Exception{
 
         Processor.clockCycles += 50;
 
         int addr = address.getSigned();
-
         int index = searchAddresses(addr);
 
         // If it exists, update it.
@@ -46,18 +62,28 @@ public class L2Cache{
         MainMemory.write(address, value);
     }
 
+    /**
+     * Updates the InstructionCache cache.
+     * 
+     * Costs 50 cycles on a hit and 400 on a miss.
+     * 
+     * @param address The address of the desired Instruction.
+     * @return A tuple containing the 8 words, and the start address for the 8 words.
+     * @throws Exception
+     */
     public static Tuple getInstructions(Word address) throws Exception{
-        int addr = address.getSigned();
-
+        // Only add 40 because InstructionCache already adds 10.
         Processor.clockCycles += 40;
-
+        
+        int addr = address.getSigned();
         int index = searchAddresses(addr);
 
         if (index == -1){
+            // Add another 350 to get to 400.
             Processor.clockCycles += 350;
 
+            // Get the next cache. Loops from 0-3 infinitly (or at least infintly enough).
             index = nextCache++ % CACHE_COUNT;
-
             addresses[index] = addr;
 
             for(int i = 0; i<CACHE_SIZE && addr + i < MainMemory.MEM_SIZE; i++){
@@ -68,6 +94,9 @@ public class L2Cache{
         return new Tuple(cache[index], addresses[index]);
     }
 
+    /**
+     * Basicly a struct that I can pass back and forth.
+     */
     public static class Tuple{
         public Word[] words;
         public int address;
@@ -78,11 +107,18 @@ public class L2Cache{
         }
     }
 
+    /**
+     * Checks if there is already a cache with the desired Memory.
+     * 
+     * @param address The address of the desired Word in memory.
+     * @return The cache index of the desired Word, or -1 if it's not found.
+     */
     public static int searchAddresses(int address){
 
         int index = -1;
 
         for(int i = 0; i<CACHE_COUNT; i++){
+            // Check if the address is within 8 Words after a starting address.
             if (address >= addresses[i] && address < addresses[i] + CACHE_SIZE){
                 index = i;
                 break;
@@ -92,6 +128,9 @@ public class L2Cache{
         return index;
     }
 
+    /**
+     * Clears the cache for use by another process.
+     */
     public static void clear(){
         for(int i = 0; i<CACHE_COUNT; i++){
             for(int j=0; j<CACHE_SIZE; j++){
